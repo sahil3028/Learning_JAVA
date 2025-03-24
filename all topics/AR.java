b5d7c4b98fa12f00317eebbf72d34258d493ae6a
Mayukh <mayukhpal.mapl.com>
2025-03-24 23:50:01 +0530

    Updated array q2

---
+++ all topics/AR.java
@@ -0,0 +1,10 @@
+public class AR {
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5};
+        int sum = 0;
+        for (int number : numbers) {
+            sum += number;
+        }
+        System.out.println("The sum of the array is: " + sum);
+    }
+}
