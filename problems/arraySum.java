9deb71aeeabcd3073c420600d71377e1ada21168
Soyab <itssoyab18@gmail.com>
2025-03-24 14:56:01 +0530

    updated for sum in new class for the array element addition

--- problems/arraySum.java
+++ problems/arraySum.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class issue_sol {
+public class arraySum {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
@@ -8,13 +8,11 @@ public class issue_sol {
         int n = sc.nextInt();
 
         int[] arr = new int[n];
-        int sum = 0;
-
         System.out.println("Enter " + n + " elements:");
         for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
-            sum += arr[i];  // Calculate sum
         }
+        int sum=Addition.Sum(arr,n);
         System.out.println("Sum of array elements: " + sum);
     }
 }
