42f9fb900ea91942a87616c544998ec0c7116c48
Sahil Sah <sahil12345work@gmail.com>
2025-03-28 01:58:24 +0530

    Delete ProblemsMayukh directory
    it was for teaching purposes

--- ProblemsMayukh/Sum.java
+++
@@ -1,9 +0,0 @@
-public class Sum {
-    public static int Add(int []arr, int n){
-        int sum=0;
-        for(int i=0;i<n;i++) {
-            sum += arr[i];
-        }
-        return sum;
-    }
-}
