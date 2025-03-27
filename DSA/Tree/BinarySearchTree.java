e303fe41f0404a4ebd84f7e399c5f84c1e4dcef0
Sahil Sah <sahil12345work@gmail.com>
2025-03-28 04:04:13 +0530

    added Binary Tree topic

--- DSA/Tree/BinarySearchTree.java
+++ DSA/Tree/BinarySearchTree.java
@@ -1,4 +1,60 @@
 package Tree;
 
 public class BinarySearchTree {
+    class Node{
+        int val;
+        Node left;
+        Node right;
+
+        private Node(int val) {
+            this.val = val;
+        }
+    }
+
+    Node root;
+
+    public boolean Insert(int val){
+        Node newn =new Node(val);
+        if(root==null){
+            root=newn;
+            return true;
+        }
+        Node temp=root;
+        while(true){
+            if(temp.val== newn.val){
+                return false;
+            }
+            if(newn.val<temp.val){
+                if(temp.left==null){
+                    temp.left=newn;
+                    return true;
+                }
+                temp=temp.left;
+            }else{
+                if(temp.right==null){
+                    temp.right=newn;
+                    return true;
+                }
+                temp=temp.right;
+            }
+        }
+    }
+
+    public boolean Contains(int x){
+        if(root==null)return false;
+        if(root.val==x) return true;
+
+        Node temp= root;
+
+        while(temp!=null){
+            if(temp.val==x) return true;
+
+            if(x>temp.val)
+                temp=temp.right;
+            else
+                temp=temp.left;
+
+        }
+        return false;
+    }
 }
--- DSA/Tree/Main.java
+++ DSA/Tree/Main.java
@@ -1,4 +1,17 @@
 package Tree;
 
 public class Main {
+    public static void main(String[] args) {
+        BinarySearchTree bst=new BinarySearchTree();
+
+        bst.Insert(45);
+        bst.Insert(88);
+        bst.Insert(56);
+        bst.Insert(34);
+        bst.Insert(23);
+        bst.Insert(9);
+
+        System.out.println(bst.Contains(49));
+
+    }
 }
