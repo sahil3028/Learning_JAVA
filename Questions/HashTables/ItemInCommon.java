97477193b4bfa1d59bfb6ec8295f84ea52a1eada
Sahil Sah <sahil12345work@gmail.com>
2025-03-30 11:18:12 +0530

    Item in common

---
+++ Questions/HashTables/ItemInCommon.java
@@ -0,0 +1,22 @@
+package HashTables;
+
+import java.util.HashMap;
+
+public class ItemInCommon {
+    public boolean sol(int[] a1 , int[] a2){
+        public boolean itemInCommon(int[] array1, int[] array2) {
+            HashMap<Integer, Boolean> myHashMap = new HashMap<>();
+
+            for (int i : array1) {
+                myHashMap.put(i, true);
+            }
+
+            for (int j : array2) {
+                if (myHashMap.get(j) != null) return true;
+            }
+
+            return false;
+        }
+
+    }
+}
